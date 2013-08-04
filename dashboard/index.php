eb5a8f5b1d893400fb15f541ba28085568810e1c
Isaac White <iaw2105@columbia.edu>
2013-08-04 02:12:53 -0400

    Basic functionality working, but I think I need to switch to another icon source; it looks like the moon for this set of icons is cut off.

--- dashboard/index.php
+++ dashboard/index.php
@@ -4,6 +4,46 @@ $today = date("mdy"); //determine the current date as a 6 character string
 $todayString = date("m/d/y");
 $file = 'useCount.txt';
 $zipCode = 10027;
+
+function getWeatherElements($iconCode,$isDay) {
+    $iconHTML = "<ul class=\"weather\"><li class=\"";
+    if ($iconCode == "cloudy") {
+        $iconHTML = "cloud";
+    } else if (($iconCode == "chancerain") ||
+            ($iconCode == "rain")) {
+        $iconHTML = "rainy";
+    } else if (($iconCode == "chanceflurries") ||
+        ($iconCode == "chancesnow") ||
+        ($iconCode == "flurries") ||
+        ($iconCode == "snow")) {
+
+        $iconHTML = "snowy";
+    } else if (($iconCode == "sleet") ||
+        ($iconCode == "chancesleet")) {
+        $iconHTML = "sleet";
+    } else if (($iconCode == "chancetstorms") ||
+        ($iconCode == "tstorms")) {
+        $iconHTML = "thunder";
+    } else {
+        if($isDay) {
+            $iconHTML .= "icon-sun\"></li></ul>";
+        } else {
+            $iconHTML .= "icon-moon\"></li></ul>";
+        }
+    }
+
+    return $iconHTML;
+}
+
+//actual api lookup
+$forcastLookup = "http://api.wunderground.com/api/ae1ee363833e1943/forecast/q/";
+$conditionsLookup = "http://api.wunderground.com/api/ae1ee363833e1943/geolookup/conditions/q/";
+
+//some saved responses for testing
+// $forcastLookup = "forcast_";
+// $conditionsLookup = "conditions_";
+
+
 // Open the file to get previous page info
 $useString = file_get_contents($file);
 
@@ -28,12 +68,57 @@ $newUseString .= $today . "," . $queryCount;
 // Write the contents back to the file
 file_put_contents($file, $newUseString);
 
-$json_string = file_get_contents("http://api.wunderground.com/api/ae1ee363833e1943/forecast/q/" . $zipCode . ".json");
-$json_conditions = file_get_contents("http://api.wunderground.com/api/ae1ee363833e1943/geolookup/conditions/q/" . $zipCode . ".json");
-$parsed_json = json_decode($json_string);
-$location = $parsed_json->{'location'}->{'city'};
+$jsonForcast = file_get_contents($forcastLookup . $zipCode . ".json");
+$jsonConditions = file_get_contents($conditionsLookup . $zipCode . ".json");
+$parsedForcast = json_decode($jsonForcast);
+$parsedConditions = json_decode($jsonConditions); 
+/*$location = $parsed_json->{'location'}->{'city'};
 $temp_f = $parsed_json->{'current_observation'}->{'temp_f'};
+$currentHigh = $parse_json->*/
+$currentObservation = $parsedConditions ->{'current_observation'};
+$forcasts = $parsedForcast->{'forecast'}->{'txt_forecast'}->{'forecastday'};
+$simpleForcasts = $parsedForcast->{'forecast'}->{'simpleforecast'}->{'forecastday'};
+$currentTemp = $currentObservation->{'temp_f'};
+$feelsLike = $currentObservation->{'feelslike_f'};;
+$conditionsKey = $currentObservation->{'icon'};
+$iconClass = getWeatherElements($conditionsKey);
+$lastUpdated = $currentObservation->{'observation_time'};
+
+$previewArray = array();
+$forecastIcons = array();
+
+foreach($forcasts as $period) {
+    $title = $period->{'title'};
+    $icon = $period->{'icon'};
+    if(!stristr($title,'night')) {
+         $daytime = TRUE;
+    } else {
+        $daytime = FALSE;
+    }
+
+    $iconClean = getWeatherElements($icon,$daytime);
+    array_push($previewArray, $title);
+    array_push($forecastIcons, $iconClean);
+}
+$forecastTemps = array();
+foreach($simpleForcasts as $period) {
+    $high = $period->{'high'}->{'fahrenheit'};
+    $low = $period->{'low'}->{'fahrenheit'};
+    //if the first forecast is an evening value
+
+    if(!stristr($previewArray[0],'night')) {
+         array_push($forecastTemps,$high,$low);
+    } else {
+        array_push($forecastTemps,$low,$high);
+    }
+}
+
+//var_dump($previewArray);
+// var_dump($forcasts);
+
 ?>
+
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -50,12 +135,42 @@ $temp_f = $parsed_json->{'current_observation'}->{'temp_f'};
         <link rel="stylesheet" href="../css/style.css">
 
         <script src="../js/vendor/modernizr-2.6.2.min.js"></script>
+        <link rel="stylesheet" href="../fonts/weather/iconvault-preview.css">
+        <style>
+        .weather {
+            font-size: 14rem;
+            text-align: center;
+            color: orange;
+        }
+        .weather p {
+            font-size: 4rem;
+            margin: 0;
+            color: white;
+        }
+        .preview {
+            float: left;
+            width: 25%;
+        }
+        ul {
+            padding: 0;
+            margin: 0;
+        }
+        li {
+            display: block;
+            text-align: center;
+
+        }
+        li:after {
+            content: "";
+        }
+        </style>
         <script>
+
         // var date = "Date: " + <?php print $todayString; ?>;
         // var fileOut = "File output: \"" + <?php print $newUseString; ?> + "\"";
         var queryCount = "Daily Query Count: " + <?php print $queryCount; ?> ;
-        var serverResponse = <?php print $json_string;?>;
-        var conditionResponse = <?php print $json_conditions; ?>;
+        var serverResponse = <?php print $jsonForcast;?>;
+        var conditionResponse = <?php print $jsonConditions; ?>;
         console.log(serverResponse);
         console.log(conditionResponse);
         // console.log(date);
@@ -68,9 +183,32 @@ $temp_f = $parsed_json->{'current_observation'}->{'temp_f'};
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
         <div id="dashboard">
-        	<h3>Date: <?php print $todayString ?></h3>
-        	<h3>File output: "<?php print $newUseString ?>"</h3>
-        	<h3>Daily Query Count : <?php print $queryCount ?> </h3>
+        	<h3>Current Temperature: <?php print $currentTemp; ?></h3>
+        	<h3>Feels Like: <?php print $feelsLike; ?></h3>
+        	<h3>Condition: <?php print $conditionsKey; ?></h3>
+            <h3><?php print $lastUpdated ?></h3>
+            <?php print $iconClass; ?>
+                <p><?php print $currentTemp?> °F</p>
+        </div>
+        <div class="preview first">
+            <h3><?php print $previewArray[0]; ?></h3>
+              <?php print $forecastIcons[0]; ?>
+            <p><?php print $forecastTemps[0]; ?> °F</p>
+        </div>
+        <div class="preview second">
+             <h3><?php print $previewArray[1];  ?></h3>
+             <?php print $forecastIcons[1]; ?>
+             <p><?php print $forecastTemps[1]; ?> °F</p>
+        </div>
+        <div class="preview third">
+            <h3><?php print $previewArray[2];  ?></h3>
+                <?php print $forecastIcons[2]; ?>
+             <p><?php print $forecastTemps[2]; ?> °F</p>
+        </div>
+        <div class="preview fourth">
+             <h3><?php print $previewArray[3];  ?></h3>
+             <?php print $forecastIcons[3]; ?>
+             <p><?php print $forecastTemps[3]; ?> °F</p>
         </div>
 
     </body>
