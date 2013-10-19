dcf80821debaf079ca767cc03bcc4ffb9c1bcd6c
Isaac White <iaw2105@columbia.edu>
2013-10-19 17:53:39 -0400

    Fixed dragging bug, and prevented edges of selection from displaying. Update for more accurate positioning pending.

--- js/main.js
+++ js/main.js
@@ -2,46 +2,18 @@ var IW = {animating:false};
 IW.animationID = undefined;
 IW.animationRotation = 0;
 IW.dragging = undefined;
+IW.carouselCenter = undefined;
 
 
-$(".web").click(function(e) {
-	window.history.pushState(null,null,"/portfolio/web")
- 	e.preventDefault();
-	$.get("/portfolio/ajax/web.html")
-	 .done(function( data ) {
-	    $(".web-region").html(data);
-	 }, false);
-});
-
-$(".print").click(function(e) {
-	window.history.pushState(null,null,"/portfolio/print")
- 	e.preventDefault();
-	$.get("/portfolio/ajax/print.html")
-	 .done(function( data ) {
-	    $(".print-region").html(data);
-	 }, false);
-	
-});
-$(".art").click(function(e) {
-	window.history.pushState(null,null,"/portfolio/art")
- 	e.preventDefault();
-	$.get("/portfolio/ajax/art.html")
-	 .done(function( data ) {
-	    $(".art-region").html(data);
-	 }, false);
-});
-
-IW.currentWinWidth = $(window).width();
-IW.currentWinHeight = $(window).height();
-
-IW.getCircularPositioning= function(percent,radiusPx,centerPos) {
-	percent = (percent + IW.animationRotation)%1 - .25;
-	if (.25 < percent && percent < .75) {
+IW.getCircularPositioning = function(percent,radiusPx,centerPos) {
+	percent = (IW.animationRotation + percent)%1 - .25;
+	// console.log(percent);
+	if (!(-.25 < percent && percent < .25)) {
 		percent = percent - .5;
 	}
 	var radAngle = percent * 2 * Math.PI - (.5 * Math.PI);
 	var xValue = radiusPx * Math.cos(radAngle) + radiusPx;
-	var yValue = radiusPx * Math.sin(radAngle) + radiusPx;
+	var yValue = radiusPx * Math.sin(radAngle) + radiusPx + 20;
 	var coord = {x:xValue,y:yValue};
 	var rotAngle = percent * 360;
 
@@ -49,45 +21,36 @@ IW.getCircularPositioning= function(percent,radiusPx,centerPos) {
 	return result;
 }
 
-$(document).ready(function () {
-	var container = $(".web-region ul");
-	var test = container.children();
-	var itemCount = test.length;
+IW.setRotation = function(degRotation) {
+	IW.animationRotation =+ degRotation/360;
+	IW.degreeRotation = degRotation;
+	var container = IW.carousel;
+	var children = container.children();
+	var itemCount = children.length;
 	var radius = ((IW.currentWinWidth-200)/2);
 	var centerX = IW.currentWinWidth/2;
-	var centerY = IW.currentWinHeight/2;
+	var centerY = (IW.currentWinHeight/2) + 20;
 	var thisCenter = {x:centerX, y:centerY};
+	IW.carouselCenter = thisCenter;
 	var itemWidth = $(".web-region ul li").width();
-
 	container.css({
 		width: IW.currentWinWidth - 200,
 		height: radius/2
 	});
-
-	for(var i = 0; i < test.length; i++){
+	for(var i = 0; i < children.length; i++){
 		var itemNumber = i;
-		var percent = itemNumber/test.length;
-		var returnVal = IW.getCircularPositioning(percent,radius,thisCenter,IW.animationRotation);
+		var percent = itemNumber/(children.length * 2);
+		var returnVal = IW.getCircularPositioning(percent,radius,thisCenter);
 		var rotateString = "rotate(" + returnVal.rot + "deg)";
 		var xPosition = returnVal.loc.x - itemWidth/2;
-		$(test[i]).css({
+		var yPosition = returnVal.loc.y;
+		$(children[i]).css({
 			position:"absolute",
 			left:xPosition,
-			top:returnVal.loc.y,
+			top: yPosition,
 			transform: rotateString
 		});
 	}
-	
-});
-IW.setRotation = function(jqueryContainer) {
-	var container = jqueryContainer;
-	var chilren = container.children();
-	var itemCount = test.length;
-	var radius = ((IW.currentWinWidth-200)/2);
-	container.css({
-		width: IW.currentWinWidth - 200,
-		height: radius/2
-	});
 }
 
 IW.startAnimation = function() {
@@ -98,6 +61,7 @@ IW.startAnimation = function() {
 	var centerX = IW.currentWinWidth/2;
 	var centerY = IW.currentWinHeight/2;
 	var thisCenter = {x:centerX, y:centerY};
+	IW.carouselCenter = thisCenter;
 	var itemWidth = $(".web-region ul li").width();
 	console.log(itemWidth);
 	container.css({
@@ -126,16 +90,44 @@ IW.startAnimation = function() {
 	return id;	
 }
 
-IW.animateWithMouse = function() {
-	$(document).mousemove(function(e){
-		IW.animationID = setTimeout(function() {
-			var xPos = e.pageX;
-			var yPos = e.pageY;
+IW.animateWithMouse = function(xPos,yPos) {
 
-
-		},17);
-	});
+	var xCenter = IW.carouselCenter.x;
+	var yCenter = IW.carouselCenter.y;
+	var deltaX = xPos - xCenter;
+	var deltaY = yPos - yCenter;
+	var angle = Math.atan(deltaY/deltaX) * (180/Math.PI);
+	IW.setRotation(angle);
 }
+
+//EVENT HANDLERS
+
+$(".web").click(function(e) {
+	window.history.pushState(null,null,"/portfolio/web")
+ 	e.preventDefault();
+	$.get("/portfolio/ajax/web.html")
+	 .done(function( data ) {
+	    $(".web-region").html(data);
+	 }, false);
+});
+
+$(".print").click(function(e) {
+	window.history.pushState(null,null,"/portfolio/print")
+ 	e.preventDefault();
+	$.get("/portfolio/ajax/print.html")
+	 .done(function( data ) {
+	    $(".print-region").html(data);
+	 }, false);
+	
+});
+$(".art").click(function(e) {
+	window.history.pushState(null,null,"/portfolio/art")
+ 	e.preventDefault();
+	$.get("/portfolio/ajax/art.html")
+	 .done(function( data ) {
+	    $(".art-region").html(data);
+	 }, false);
+});
 
 $(".toggle-animation").click(function() {
 	if(!IW.animating) {
@@ -149,13 +141,41 @@ $(".toggle-animation").click(function() {
 
 $(".web-region ul li").mousedown(function() {
 	IW.dragging = true;
-	IW.animateWithMouse();
+	$(document).mousemove(function(e){
+		setTimeout(function() {
+			var xPos = e.pageX;
+			var yPos = e.pageY;
+			IW.animateWithMouse(xPos,yPos);
+			$(".print-region h2").html(IW.degreeRotation);
+		},17);
+	});
 });
 
 $(document).mouseup(function() {
 	if (IW.dragging) {
+		$(document).off("mousemove");
 		IW.dragging = false;
-		clearInterval(IW.animationID);
-		alert("Stopped dragging");
 	}
+});
+
+IW.currentWinWidth = $(window).width();
+IW.currentWinHeight = $(window).height();
+
+$(document).ready(function () {
+	IW.carousel = container = $(".web-region ul");
+	var test = container.children();
+	var itemCount = test.length;
+	var radius = ((IW.currentWinWidth-200)/2);
+	var centerX = IW.currentWinWidth/2;
+	var centerY = IW.currentWinHeight/2;
+	var thisCenter = {x:centerX, y:centerY};
+	var itemWidth = $(".web-region ul li").width();
+
+	container.css({
+		width: IW.currentWinWidth - 200,
+		height: radius/2
+	});
+
+	IW.setRotation(270);
+	
 });
