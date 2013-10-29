358b1a3a21824b64cac52f32b9a4c437e0e7bc7a
Isaac White <iaw2105@columbia.edu>
2013-10-29 01:44:49 -0400

    Made the angle checking more robust, need to fix position constraint

--- js/main.js
+++ js/main.js
@@ -6,6 +6,64 @@ IW.carouselCenter = undefined;
 IW.cardDimensions = undefined;
 
 //function to calculate coordinates of element on circle
+//simple enough, though not sure if it should be modeled as some kind of other obj
+IW.getQuadrant = function(xPos,yPos) {
+    if (xPos > IW.carouselCenter.x) {
+        //quadrants 1 or 2
+        if (yPos > IW.carouselCenter.y) {
+            return 1;
+        } else {
+            return 2;
+        }
+    } else {
+        //quadrants 3 or 4
+        if (yPos > IW.carouselCenter.y) {
+            return 4;
+        } else {
+            return 3;
+        }
+    }
+}
+
+//this is good.
+IW.getAngle = function(xPos,yPos) {
+    //a private function
+    var getQuadrant = function(xPos,yPos) {
+        if (xPos >= IW.carouselCenter.x) {
+            //quadrants 1 or 2
+            if (yPos >= IW.carouselCenter.y) {
+                return 2;
+            } else {
+                return 1;
+            }
+        } else {
+            //quadrants 3 or 4
+            if (yPos >= IW.carouselCenter.y) {
+                return 3;
+            } else {
+                return 4;
+            }
+        }
+    }
+
+    var quad = getQuadrant(xPos,yPos);
+    var xCenter = IW.carouselCenter.x;
+    var yCenter = IW.carouselCenter.y;
+    var deltaX = xPos - xCenter;
+    var deltaY = yPos - yCenter;
+    var rawAngle = Math.atan(deltaY/deltaX) * (180/Math.PI);
+    var adjAngle;
+
+    if (quad === 1 || quad === 2) {
+        adjAngle = 90 + rawAngle;
+    } else {
+        adjAngle = 270 + rawAngle;
+    }
+
+    return adjAngle;
+
+}
+//this could have problems.
 IW.getCircularPositioning = function (percent, radiusPx, centerPos) {
     "use strict";
     //rotate so that the first element starts at the 12 oclock position
@@ -35,9 +93,15 @@ IW.getCircularPositioning = function (percent, radiusPx, centerPos) {
     return result;
 }
 
-//
+//this could also have problems
 IW.setRotation = function (degRotation) {
     IW.degreeRotation = degRotation;
+    //degree rotation is set ot whatever rotation we request for the slider on initial call.
+
+    degRotation = degRotation + 270;
+    if (degRotation >= 360) {
+        degRotation = degRotation - 360;
+    }
 
     var container = IW.carousel;
     var children = container.children();
@@ -65,33 +129,70 @@ IW.setRotation = function (degRotation) {
         height: radius/2
     });
     
+   
     var itemCount = children.length;
-    var angleRange = 360 / (itemCount * 2);
-    //this is how many degrees to divide the circle by.
 
-    zIndexMapping = [];
-    for (var i = 0; i < itemCount; i++) {
-        //the first item always gets drawn starting from IW.animationRotation
-        var angularPosition = IW.animationRotation + (i * angleRange);
-        if (angularPosition > 90) {
-            //this should put it back into an allowed range.
-            angularPosition -= 180;
-
+    //we need to know the position of the 
+    //first element to anticipate the angle values of each item
+    var makeZIndexMap = function (itemCount) {
+         //this is how many degrees to divide the circle by.
+        var angleRange = 360 / (itemCount * 2);
+        var zIndexMapping = [];
+        var angularString = ""
+        
+        var correctAngle = function(angle) {
+            
+            if( (-90 > angle) || (angle > 90) ) {
+                //we have to correct it.
+                if (angle > 0) {
+                    while (angle > 90) {
+                        angle = angle - 180;
+                    }
+                } else {
+                    while (angle < -90) {
+                        angle = angle + 180;
+                    }
+                }
+                return angle;
+            } else {
+                return angle;
+            }
         }
-        var indexCount, zIndexVal;
-        if (angularPosition > 0) {
-            //if positive, check the distance from 0 divided by angleRange
-            indexCount = Math.floor(angularPosition/angleRange);
-            zIndexVal = 100 - indexCount;
-        } else {
-            //if negative, check the distance from 0 divided by angleRange
-            indexCount = Math.ceil(angularPosition/angleRange);
-            zIndexVal = Math.abs(indexCount);
-        }
+        
+        for (var i = 0; i < itemCount; i++) {
+            //the first item always gets drawn starting from IW.animationRotation
+            var angularPosition = (degRotation - 270) + (i * angleRange);
+            
+            //try to fix this.
+            angularPosition = correctAngle(angularPosition);
 
-        zIndexMapping[i] = zIndexVal;
+            angularString += i + ": " + angularPosition + ", ";
+            var indexCount, zIndexVal;
+            var startPos = 0 - (angleRange / 2);
+            if (angularPosition >= startPos) {
+                //if positive, check the distance from 0 divided by angleRange
+                indexCount = Math.floor(angularPosition/angleRange);
+                zIndexVal = 100 - indexCount;
+            } else {
+                //if negative, check the distance from 0 divided by angleRange
+                indexCount = Math.floor(angularPosition/angleRange);
+                zIndexVal = 50 + indexCount;
+            }
+
+            zIndexMapping[i] = zIndexVal;
+        }
+        var htmlString = "";
+        for (var i = 0; i < zIndexMapping.length; i++) {
+            htmlString += i + ": " + zIndexMapping[i] + ", ";
+        }
+        $(".art-region h2").html(htmlString).css({"margin-top":"15rem"});
+        $(".art-region h3").html(angularString);
+        return zIndexMapping;
+
     }
     
+
+    var zIndexMapping = makeZIndexMap(itemCount);
 
     for(var i = 0; i < itemCount; i++){
         var itemNumber = i;
@@ -145,14 +246,18 @@ IW.setRotation = function (degRotation) {
     }
 }
 
+//could have problems.
 IW.animateWithMouse = function (xPos,yPos) {
 
-    var xCenter = IW.carouselCenter.x;
-    var yCenter = IW.carouselCenter.y;
-    var deltaX = xPos - xCenter;
-    var deltaY = yPos - yCenter;
-    var angle = Math.atan(deltaY/deltaX) * (180/Math.PI);
-    IW.setRotation(angle + IW.animationRotation);
+    
+    var thisAngle = IW.getAngle(xPos,yPos);
+    var startAngle = IW.getAngle(IW.dragStart.x,IW.dragStart.y);
+
+    var dispAngle = thisAngle - startAngle;
+
+    console.log(thisAngle);
+    // console.log(angle);
+    IW.setRotation(dispAngle + IW.animationRotation);
     //we have to take into account the position when it started...
 }
 
@@ -215,34 +320,49 @@ $(".see-code").click( function (e) {
 });
 
 $(".web-region ul li").mousedown(function() {
-    IW.dragging = true;
     $(document).mousemove( function (e){
+        //only execute after first mousemove
+        if (!IW.dragging) {
+            IW.dragging = true;
+            //get our start position for displacement measurements
+            var xStart = e.pageX;
+            var yStart = e.pageY;
+            IW.dragStart = {x:xStart,y:yStart};
+        }
+        //now the normal checker
         setTimeout(function() {
             var xPos = e.pageX;
             var yPos = e.pageY;
+            //this references the dragstart we've already set.
             IW.animateWithMouse(xPos,yPos);
-            $(".print-region h2").html(IW.carouselCenter.x + "," + 
-                IW.carouselCenter.y);
+            // $(".print-region h2").html(IW.carouselCenter.x + "," + 
+            //     IW.carouselCenter.y);
         },17);
     });
 });
 
 $(document).mouseup(function() {
+    console.log("Mouseup!");
     if (IW.dragging) {
         $(document).off("mousemove");
         IW.dragging = false;
 
-        //set rotation.
-        if (IW.degreeRotation < -90 || IW.degreeRotation > 90) {
-            console.log("Out of bounds!");
-            if (IW.degreeRotation < -90) {
-                IW.degreeRotation += 90;
-            }
-            if (IW.degreeRotation > 90) {
-                IW.degreeRotation -= 90;
-            }
-        }
+        // set rotation.
+
+        //vodoo magic!!
+        // if (IW.degreeRotation < -90 || IW.degreeRotation > 90) {
+        //     console.log("Out of bounds!");
+        //     if (IW.degreeRotation < -90) {
+        //         IW.degreeRotation += 90;
+        //     }
+        //     if (IW.degreeRotation > 90) {
+        //         IW.degreeRotation -= 90;
+        //     }
+        // }
         IW.animationRotation = IW.degreeRotation;
+        if (IW.degreeRotation >= 360) {
+            IW.degreeRotation = IW.degreeRotation - 360;
+        }
         console.log(IW.degreeRotation)
     }
 });
@@ -271,6 +391,6 @@ $(document).ready(function () {
         height: radius / 2
     });
 
-    IW.setRotation(270);
+    IW.setRotation(0); //0 should be the 12 oclock position here.
     
 });
