27b0173827a7cb034fe48b77383af2ffd664cdf6
Isaac White <iaw2105@columbia.edu>
2013-11-30 13:57:08 -0500

    Portfolio updates

---
+++ portfolio/spellcheck/js/spellcheckCompiler/compile.js
@@ -0,0 +1,165 @@
+ var fs = require('fs');
+
+var dictionary = [];//the array that will become our bloomfilter
+var seeds = [123,321,213,312]; //seed values
+
+//read the dictionary documents
+var fileData = fs.readFileSync('words-plural.txt').toString().split("\r\n");
+var fileData2 = fs.readFileSync('words.txt').toString().split("\n");
+
+//add words in first dictionary
+for (var i = 0; i<fileData.length; i++) {
+	addHashes(fileData[i]);
+}
+
+//add words in second dictionary
+for (var i=0; i<fileData.length; i++) {
+	addHashes(fileData2[i]);
+}
+var ouch = "";
+for (var i = 0; i<dictionary.length; i++) {
+	if(dictionary[i] == 1) {
+		ouch += 1;
+	} else {
+		ouch += 0;
+	}
+}
+
+// console.log(ouch);
+var substring;
+var start, end;
+// for (var i =0; i<1024; i++) {
+// 	start = i * 1024;
+// 	end = (i + 1) * 1024;
+// 	substring = ouch.substring(start,end); //this gets 100 values, 
+// 	console.log(binToString(substring));
+// }
+
+var test = binToString("0110011100101110001010011011100011100000000111101011111010111000000000010111101110100000001100011001");
+console.log(test);
+
+function binToString(binaryString) {
+	var returnValue = "";
+	var currentBinary;
+	var start, end;
+	var charValue;
+	var valuesArray;
+	for (var i=0; i <128; i++) {
+		start = i * 8;
+		end = (i+1) * 8;
+		currentBinary = binaryString.substring(start,end);
+		valuesArray = currentBinary.split();
+		for (var j =0; i < valuesArray.length; j++) {
+			charValue += (2^(7-j) * parseInt(valuesArray[j]));
+		}
+		returnValue += String.fromCharCode(charValue);
+	}
+
+	return returnValue;
+}
+
+//write the file, filename will be dic2
+// fs.writeFile("dic2", dictionary, function(err) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log("The file was saved!");
+//     }
+// }); 
+
+
+
+//prevents hashes from going past end of array
+function getIndex(number) {
+	if (number > 1048576-1) {
+		number = number % 1048576;
+	} //no else
+	return number;
+}
+
+//hashes each seed and adds it
+function addHashes(someWord) {
+	var index;
+	//hash with the requested seeds and add to the array
+	for (var i=0; i<seeds.length;i++) {
+		index = getIndex(murmurhash3_32_gc(someWord,seeds[i]));
+		dictionary[index] = 1;
+	}
+}
+
+//checks a word
+function checkWord(someWord) {
+	var currentIndex;
+	for (var i=0; i<seeds.length;i++) {
+		currentIndex = getIndex(murmurhash3_32_gc(someWord,seeds[i]));
+		if (dictionary[currentIndex] != 1) {
+			return false
+		}
+	}
+	return true;
+}
+
+/**
+ * JS Implementation of MurmurHash3 (r136) (as of May 20, 2011)
+ * 
+ * @author <a href="mailto:gary.court@gmail.com">Gary Court</a>
+ * @see http://github.com/garycourt/murmurhash-js
+ * @author <a href="mailto:aappleby@gmail.com">Austin Appleby</a>
+ * @see http://sites.google.com/site/murmurhash/
+ * 
+ * @param {string} key ASCII only
+ * @param {number} seed Positive integer only
+ * @return {number} 32-bit positive integer hash 
+ */
+
+function murmurhash3_32_gc(key, seed) {
+	var remainder, bytes, h1, h1b, c1, c1b, c2, c2b, k1, i;
+
+	remainder = key.length & 3; // key.length % 4
+	bytes = key.length - remainder;
+	h1 = seed;
+	c1 = 0xcc9e2d51;
+	c2 = 0x1b873593;
+	i = 0;
+
+	while (i < bytes) {
+	  	k1 = 
+	  	  ((key.charCodeAt(i) & 0xff)) |
+	  	  ((key.charCodeAt(++i) & 0xff) << 8) |
+	  	  ((key.charCodeAt(++i) & 0xff) << 16) |
+	  	  ((key.charCodeAt(++i) & 0xff) << 24);
+		++i;
+
+		k1 = ((((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16))) & 0xffffffff;
+		k1 = (k1 << 15) | (k1 >>> 17);
+		k1 = ((((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16))) & 0xffffffff;
+
+		h1 ^= k1;
+        h1 = (h1 << 13) | (h1 >>> 19);
+		h1b = ((((h1 & 0xffff) * 5) + ((((h1 >>> 16) * 5) & 0xffff) << 16))) & 0xffffffff;
+		h1 = (((h1b & 0xffff) + 0x6b64) + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16));
+	}
+
+	k1 = 0;
+
+	switch (remainder) {
+		case 3: k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16;
+		case 2: k1 ^= (key.charCodeAt(i + 1) & 0xff) << 8;
+		case 1: k1 ^= (key.charCodeAt(i) & 0xff);
+
+		k1 = (((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
+		k1 = (k1 << 15) | (k1 >>> 17);
+		k1 = (((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16)) & 0xffffffff;
+		h1 ^= k1;
+	}
+
+	h1 ^= key.length;
+
+	h1 ^= h1 >>> 16;
+	h1 = (((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >>> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
+	h1 ^= h1 >>> 13;
+	h1 = ((((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >>> 16) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
+	h1 ^= h1 >>> 16;
+
+	return h1 >>> 0;
+}
